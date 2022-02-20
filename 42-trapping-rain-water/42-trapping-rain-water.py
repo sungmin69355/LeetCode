53a309df4d66b843a64dcc77eddfe76bca546f47
Cho Sungmin <sungmin69355@naver.com>
2022-02-20 12:08:19 +0900

    Time: 132 ms (34.12%), Space: 15.6 MB (72.34%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -0,0 +1,41 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+# 스택으로 풀이
+        stack = []
+        volume= 0
+        for i in range(len(height)):
+            #변곡점을 만나는경우
+            while stack and height[i]>height[stack[-1]]:
+                #스택에서 꺼낸다.
+                top = stack.pop()
+                
+                if not len(stack):
+                    break
+                # 이전과의 차이만큼 물높이 처리
+                distance = i -stack[-1] -1
+                waters = min(height[i], height[stack[-1]])- height[top]
+                
+                volume += distance *waters
+            stack.append(i)
+        return volume
+
+# 투포인터로 풀이
+#         if not height:
+#             return 0
+        
+#         volume = 0
+#         left,right=0, len(height) -1
+#         left_max, right_max = height[left], height[right]
+        
+#         while left<right:
+#             left_max, right_max = max(height[left],left_max), max(height[right],right_max)
+                
+#             #더 높은 쪽을 향해서 투포인트 이동
+#             if left_max <= right_max:
+#                 volume += left_max - height[left]
+#                 left+=1
+#             else:
+#                 volume += right_max - height[right]
+#                 right-=1
+                
+#         return volume
