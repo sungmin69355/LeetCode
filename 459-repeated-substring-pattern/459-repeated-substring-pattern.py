9cdd6e3d796bd6b61366426c560d793e5bc16aa2
Cho Sungmin <sungmin69355@naver.com>
2022-06-03 20:44:45 +0900

    Time: 997 ms (11.03%), Space: 13.8 MB (99.49%) - LeetHub

---
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.py
@@ -0,0 +1,9 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        
+        
+        for i in range(1, len(s)):
+            if s[:i] * int(len(s)//i) == s:
+                return True
+        return False
+            
