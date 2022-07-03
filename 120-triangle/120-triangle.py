fbc8da5570cd98af21b370b37ae7b1b779f5a905
Cho Sungmin <sungmin69355@naver.com>
2022-07-03 14:28:49 +0900

    Time: 143 ms (12.75%), Space: 17.4 MB (5.77%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,12 +1,16 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+#         DP로 풀기~
+#         n = len(triangle)
         
-        n = len(triangle)
+#         dp = [[0]*(n+1) for _ in range(n+1)]
         
-        dp = [[0]*(n+1) for _ in range(n+1)]
+#         for level in range(n-1,-1,-1):
+#             for i in range(level+1):
+#                 dp[level][i] = triangle[level][i] + min(dp[level+1][i], dp[level+1][i+1])
         
-        for level in range(n-1,-1,-1):
-            for i in range(level+1):
-                dp[level][i] = triangle[level][i] + min(dp[level+1][i], dp[level+1][i+1])
-        
-        return dp[0][0]
+#         return dp[0][0]
+        @cache
+        def dfs(level, i):
+            return 0 if level >= len(triangle) else triangle[level][i]+ min(dfs(level+1,i), dfs(level+1,i+1))
+        return dfs(0,0)
