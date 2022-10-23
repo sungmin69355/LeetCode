5e127402f868886fa38583b463e0afa2d2de4cc0
Cho Sungmin <sungmin69355@naver.com>
2022-10-23 18:10:54 +0900

    Time: 222 ms (63.71%), Space: 14.7 MB (9.75%) - LeetHub

---
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        result = 0
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                result += nums[i] - nums[i+1] + 1
+                nums[i+1] = nums[i] + 1
+            elif nums[i] == nums[i+1]:
+                result += 1
+                nums[i+1] = nums[i]+1
+        return result
