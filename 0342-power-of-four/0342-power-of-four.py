33196ac7db0d35da054fc173a3adabb91956ae34
Cho Sungmin <sungmin69355@naver.com>
2022-10-23 17:52:35 +0900

    Time: 57 ms (50.86%), Space: 13.9 MB (9.49%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,6 +1,9 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         
+        if n == 0:
+            return False
+        
         for i in range(n):
             if n == (4**i):
                 return True
