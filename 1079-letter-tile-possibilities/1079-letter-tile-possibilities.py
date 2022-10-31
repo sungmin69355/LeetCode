dc750fda1922e525dc4209f1b3c294dab4dad767
Cho Sungmin <sungmin69355@naver.com>
2022-10-31 21:06:14 +0900

    Time: 69 ms (93.40%), Space: 16.4 MB (45.36%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -3,6 +3,6 @@ class Solution:
         cnt = list(tiles)
         result = 0
         for i in range(len(cnt)):
-            printCnt = set(list(permutations(cnt, i+1)))
+            printCnt = set(permutations(cnt, i+1))
             result += len(printCnt)
         return result
