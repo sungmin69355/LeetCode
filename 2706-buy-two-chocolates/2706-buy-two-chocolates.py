24b79358560701b7f08094de0133bec6f3b36293
Cho Sungmin <sungmin69355@naver.com>
2023-06-05 23:43:05 +0900

    Time: 65 ms (96.84%), Space: 16.3 MB (32.23%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -2,14 +2,13 @@ from itertools import combinations
 
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        twoChocolates = list(combinations(prices, 2))
+        a=min(prices)
+        prices.remove(a)
         
-        result = []
-        for i in range(len(twoChocolates)):
-            if twoChocolates[i][0] + twoChocolates[i][1] <= money:
-                result.append(twoChocolates[i][0] + twoChocolates[i][1])
-                
-        if len(result) > 0:
-            return money - min(result)
+        b=min(prices)
+        new=money-a-b
         
-        return money
+        if(a+b>money):
+            return money
+        else:
+            return new
