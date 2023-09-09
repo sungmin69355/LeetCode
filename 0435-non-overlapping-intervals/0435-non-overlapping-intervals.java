66a920f9228b9e9e23bdae081417a4771ee59b4c
Cho Sungmin <sungmin69355@naver.com>
2023-09-09 12:16:57 +0900

    Time: 44 ms (97.23%), Space: 84.2 MB (90.92%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> (a[1] - b[1]));
+        int ans = 0;
+        int endTime = -11111111;
+        for (int i = 0;i<intervals.length;i++) {
+            if(intervals[i][0]<endTime) ans++;
+            else endTime = intervals[i][1];
+        }
+        return ans;
+
+    }
+}
